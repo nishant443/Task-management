18c295e2b863037a82c0f38737322e13edc77c82
Nishant Singh <nishu55647@gmail.com>
2025-10-04 15:14:38 +0530

    updated code

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://gentle-biscotti-d5f7f7.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api", // your backend URL
+    baseURL: "http://localhost:5000/api",
 });
 
 // Automatically attach token if available
